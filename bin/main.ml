65d9ef498040df4915bbd95c1b6221509d393c3f
Humza Shahid <humza@pop-os.localdomain>
2023-03-28 16:25:32 +0100

    move app loop into lib and get fonts to be correctly loaded by dune

--- bin/main.ml
+++ bin/main.ml
@@ -77,24 +77,4 @@ let placeholder model =
             ];
         ] )
 
-let setup () =
-  Raylib.set_config_flags [ Window_resizable; Vsync_hint ];
-  Raylib.init_window 0 0 "raylib [core] example - basic window";
-  Raylib.maximize_window ();
-  ()
-
-let rec loop view (model : 'a) state =
-  match Raylib.window_should_close () with
-  | true -> Raylib.close_window ()
-  | false ->
-      let current_view = view model in
-      let open Raylib in
-      begin_drawing ();
-      clear_background (Color.create 243 243 243 255);
-      let state, model = Reyml.Drawable.draw model current_view state in
-      end_drawing ();
-      loop view model state
-
-let () =
-  let _ = setup () in
-  loop placeholder initial_model Reyml.initial_state
+let () = Reyml.run_app placeholder initial_model
--- lib/reyml.ml
+++ lib/reyml.ml
@@ -2,3 +2,21 @@ module Drawable = Drawable
 module Fluent = Fluent
 
 let initial_state = State_tree.SE
+
+let rec loop view (model : 'a) state =
+  match Raylib.window_should_close () with
+  | true -> Raylib.close_window ()
+  | false ->
+      let current_view = view model in
+      let open Raylib in
+      begin_drawing ();
+      clear_background (Color.create 243 243 243 255);
+      let state, model = Drawable.draw model current_view state in
+      end_drawing ();
+      loop view model state
+
+let run_app view initial_model =
+  Raylib.set_config_flags [ Window_resizable; Vsync_hint ];
+  Raylib.init_window 0 0 "raylib [core] example - basic window";
+  Raylib.maximize_window ();
+  loop view initial_model initial_state
