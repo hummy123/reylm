f35c648b3b0887efa3612a6c8b6b1d8e53b863b9
Humza Shahid <humza@pop-os.localdomain>
2023-03-30 08:40:53 +0100

    code row center

--- bin/main.ml
+++ bin/main.ml
@@ -6,6 +6,7 @@ type model = { counter : int }
 let initial_model = { counter = 0 }
 
 let placeholder _ =
+  ColumnCenter [
   RowCenter
     [
       Rect (100, 100, 0.3, Raylib.Color.red, Empty);
@@ -14,5 +15,6 @@ let placeholder _ =
       Rect (100, 100, 0.3, Raylib.Color.red, Empty);
       Rect (100, 100, 0.3, Raylib.Color.red, Empty);
     ]
+  ]
 
 let () = Reyml.run_app "Test" placeholder initial_model
