6f9710506cc9eb2f84121a77606c0f16faed6886
Humza Shahid <humza@pop-os.localdomain>
2023-04-02 14:23:05 +0100

    progress with listview (very start)

--- lib/listview/fluent_listview.ml
+++ lib/listview/fluent_listview.ml
@@ -1,37 +1,18 @@
 let widget parent_x parent_y parent_w parent_h state_tree model =
   let str =
-    "wertyuio\n\
-    \  ohugyuhjklk\n\
-    \  op;[]lopo\n\
-    \  llp[k\n\
-    \  l]\n\
-    \  op]lpkl[]lpl[;lpll]lpl\n\
-    \  k\n\
-    \  [k\n\
-    \  [pk[\n\
-    \    pk[\n\
-    \      kp\n\
-    \      k\n\
-    \      [pk[pk[\n\
-    \        kp[\n\
-    \          k\n\
-    \          [pk[pk[pk[\n\
-    \            kp[\n\
-    \              pk\n\
-    \              [pk[k\n\
-    \              [k[\n\
-    \                k[\n\
-    \                  k\n\
-    \                  [pk[kp[k[pk[\n\
-    \                    k\n\
-    \                    [k\n\
-    \                    [k[\n\
-    \                      k[\n\
-    \                        k[k[\n\
-    \                          k[\n\
-    \                            k"
+    "wertyu
+    [ok[pk[
+      pl[pp[kp[k[p
+      k[p
+      k[pk[
+        pk[
+          pk[
+            pk[
+              k[
+                k[pk[
+                  pk[pki"
   in
-  let img = Raylib.gen_image_text parent_w parent_h str in
+  let img = Raylib.image_text str 20 Raylib.Color.raywhite in
   let texture = Raylib.load_texture_from_image img in
   Raylib.draw_texture texture parent_x parent_y Raylib.Color.red;
   (parent_w, parent_h, state_tree, model)
