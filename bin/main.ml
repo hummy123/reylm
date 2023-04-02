30b19acb447904437c1bcdbe3b4a0b656e6e696b
Humza Shahid <humza@pop-os.localdomain>
2023-04-02 06:56:39 +0100

    add support for different fonts

--- bin/main.ml
+++ bin/main.ml
@@ -5,34 +5,9 @@ type model = { counter : int }
 
 let initial_model = { counter = 0 }
 
-let placeholder _ =
-  ColumnSpaceBetween
-    [
-      RowStart
-        [
-          Rect (100, 100, 0.3, Raylib.Color.red, Empty);
-          Rect (100, 100, 0.3, Raylib.Color.red, Empty);
-          Padding (50, 50, 50, 50, Fluent.button "123" ~text:"Standard button");
-          Rect (100, 100, 0.3, Raylib.Color.red, Empty);
-          Rect (100, 100, 0.3, Raylib.Color.red, Empty);
-          Rect (100, 100, 0.3, Raylib.Color.red, Empty);
-        ];
-      RowCenter
-        [
-          Rect (100, 100, 0.3, Raylib.Color.red, Empty);
-          Rect (100, 100, 0.3, Raylib.Color.red, Empty);
-          Rect (100, 100, 0.3, Raylib.Color.red, Empty);
-          Rect (100, 100, 0.3, Raylib.Color.red, Empty);
-          Rect (100, 100, 0.3, Raylib.Color.red, Empty);
-        ];
-      RowEnd
-        [
-          Rect (100, 100, 0.3, Raylib.Color.red, Empty);
-          Rect (100, 100, 0.3, Raylib.Color.red, Empty);
-          Rect (100, 100, 0.3, Raylib.Color.red, Empty);
-          Rect (100, 100, 0.3, Raylib.Color.red, Empty);
-          Rect (100, 100, 0.3, Raylib.Color.red, Empty);
-        ];
-    ]
+let placeholder model =
+      RowCenter [
+        Fluent.text (Format.sprintf "Clicked %i times!" model.counter);
+      ]
 
 let () = Reyml.run_app "Test" placeholder initial_model
