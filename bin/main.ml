94f3274180772eea96cc28421bd90ebf83051a4d
Humza Shahid <humzasaur@gmail.com>
2023-04-10 07:12:20 +0100

    amend align calculation

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,6 @@
 open Reyml
 
-let view = Align.widget ~x_shift:0.1 (Rect.widget ~width:60 ~height:30 ())
+let view =
+  Align.widget ~x_shift:0.0 ~y_shift:0.0 (Rect.widget ~width:60 ~height:30 ())
+
 let () = run_app view
