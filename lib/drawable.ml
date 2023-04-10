5de9766308d104840e97485cd5bf3513ebab46fa
Humza Shahid <humzasaur@gmail.com>
2023-04-10 14:43:37 +0100

    add flex type to drawable

--- lib/drawable.ml
+++ lib/drawable.ml
@@ -12,19 +12,40 @@ type input_constraints = {
 }
 
 type drawable_size = { width : int; height : int }
+type flex_fit = ForceFill | NoForce
 
 type drawable =
   | Empty
+  | Flex of int * flex_fit * drawable
   | Widget of
       (input_constraints -> drawable_size)
       * (input_constraints -> drawable_size)
 
 let empty_size = { width = 0; height = 0 }
 
-let size contraints = function
+let rec size constraints = function
   | Empty -> empty_size
-  | Widget (_, f_size) -> f_size contraints
+  | Widget (_, f_size) -> f_size constraints
+  (* Reminder: We should never call Drawable.size on a Row/Column or any Widget that accepts multiple children
+     before checking total flex of all children. *)
+  | Flex (_, fit, child) -> (
+      match fit with
+      | ForceFill ->
+          { width = constraints.max_width; height = constraints.max_height }
+      | NoForce -> size constraints child)
 
-let draw constraints = function
+let rec draw constraints = function
   | Empty -> empty_size
   | Widget (f_draw, _) -> f_draw constraints
+  | Flex (_, fit, child) -> (
+      match fit with
+      | ForceFill ->
+          let constraints =
+            {
+              constraints with
+              min_width = constraints.max_height;
+              min_height = constraints.max_height;
+            }
+          in
+          draw constraints child
+      | NoForce -> draw constraints child)
