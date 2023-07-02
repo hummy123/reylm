c860fecc98ff2e75a9860e99ac93f60080c3fc30
humza <humza@pop-os.localdomain>
2023-07-02 09:52:01 +0100

    add sized_box, change spacer to use it

---
+++ lib/sized_box.ml
@@ -0,0 +1,17 @@
+open Drawable
+
+let size width height constraints =
+  let width =
+    if constraints.min_width > width then constraints.min_width
+    else if constraints.max_width < width then constraints.max_width
+    else width
+  in
+  let height =
+    if constraints.min_height > height then constraints.min_height
+    else if constraints.max_height < height then constraints.max_height
+    else height
+  in
+  { width; height }
+
+let draw width height constraints = size width height constraints
+let widget ~width ~height () = Widget (draw width height, size width height)
--- lib/spacer.ml
+++ lib/spacer.ml
@@ -1,8 +1,8 @@
 let expand ?(flex_val = 1.) () =
-  Flex.expand ~flex_val (Rect.widget ~width:0 ~height:0 ())
+  Flex.expand ~flex_val (Sized_box.widget ~width:0 ~height:0 ())
 
 let vertical ?(flex_val = 1.) () =
-  Flex.fill_height ~flex_val (Rect.widget ~width:0 ~height:0 ())
+  Flex.fill_height ~flex_val (Sized_box.widget ~width:0 ~height:0 ())
 
 let horizontal ?(flex_val = 1.) () =
-  Flex.fill_width ~flex_val (Rect.widget ~width:0 ~height:0 ())
+  Flex.fill_width ~flex_val (Sized_box.widget ~width:0 ~height:0 ())
