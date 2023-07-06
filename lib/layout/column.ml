82eb9da5fb02affadc074c2b394aff6e21a7a78a
humza <humza@pop-os.localdomain>
2023-07-06 10:21:34 +0100

    refactor padding for update

--- lib/layout/column.ml
+++ lib/layout/column.ml
@@ -1,17 +1,25 @@
+open Constraints
 open Drawable
 
 let min children =
   Widget
-    (Column_row.min_draw children Column, Column_row.min_size children Column)
+    ( Column_row.min_draw children Column,
+      Column_row.min_size children Column,
+      Column_row.min_update children Column )
 
 let top ?(collapse_width = true) children =
   Widget
     ( Column_row.directional_draw Column_row.calc_when_start collapse_width
         children Column,
-      Column_row.max_size collapse_width children Column )
+      Column_row.max_size collapse_width children Column,
+      Column_row.directional_update Column_row.calc_when_start collapse_width
+        children Column )
 
 (* Functions for drawing row aligned to center. *)
-let calc_start_y_center constraints flex_data =
+let calc_start_y_center (constraints : input_constraints) flex_data =
+  (constraints.max_height / 2) - (flex_data.occupied_non_flex_height / 2)
+
+let update_calc_start_y_center constraints flex_data =
   (constraints.max_height / 2) - (flex_data.occupied_non_flex_height / 2)
 
 let center ?(collapse_width = true) children =
@@ -19,7 +27,10 @@ let center ?(collapse_width = true) children =
     ( Column_row.directional_draw
         (Column_row.calc_when_center Column)
         collapse_width children Column,
-      Column_row.max_size collapse_width children Column )
+      Column_row.max_size collapse_width children Column,
+      Column_row.directional_update
+        (Column_row.update_calc_when_center Column)
+        collapse_width children Column )
 
 (* Functions for drawing row aligned to right. *)
 let bottom ?(collapse_width = true) children =
@@ -27,15 +38,20 @@ let bottom ?(collapse_width = true) children =
     ( Column_row.directional_draw
         (Column_row.calc_when_end Column)
         collapse_width children Column,
-      Column_row.max_size collapse_width children Column )
+      Column_row.max_size collapse_width children Column,
+      Column_row.directional_update
+        (Column_row.update_calc_when_end Column)
+        collapse_width children Column )
 
 (* Functions for drawing row with space in between/around. *)
 let space_between ?(collapse_width = true) children =
   Widget
     ( Column_row.draw_space_between collapse_width children Column,
-      Column_row.max_size collapse_width children Column )
+      Column_row.max_size collapse_width children Column,
+      Column_row.update_space_between collapse_width children Column )
 
 let space_around ?(collapse_width = true) children =
   Widget
     ( Column_row.draw_space_around collapse_width children Column,
-      Column_row.max_size collapse_width children Column )
+      Column_row.max_size collapse_width children Column,
+      Column_row.update_space_around collapse_width children Column )
