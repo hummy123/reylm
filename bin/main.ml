97b48a63bf35f57b3dbc8c3dd9012fac58c96eab
humza <humza@pop-os.localdomain>
2023-07-07 13:02:48 +0100

    checkpoint progress

--- bin/main.ml
+++ bin/main.ml
@@ -3,26 +3,23 @@ open Reyml
 type dir = Up | Down
 type model = { dir : dir; y_pos : float }
 
-let initial = { dir = Up; y_pos = 0.0 }
-let k = Key.create ()
+let initial = { dir = Down; y_pos = 0.0 }
+
+let animate model =
+  match model.dir with
+  | Up ->
+      if model.y_pos >= -1.0 then { model with y_pos = model.y_pos -. 0.015 }
+      else { model with dir = Down }
+  | Down ->
+      if model.y_pos <= 1.0 then { model with y_pos = model.y_pos +. 0.015 }
+      else { model with dir = Up }
 
 let view model =
-  Align.widget ~y_shift:model.y_pos
-    (Row.left
-       [|
-         Rect.widget ~width:100 ~height:100 ~color:Raylib.Color.gray Empty;
-         Conditional.exec
-           (fun _ -> true)
-           (fun model ->
-             match model.dir with
-             | Up ->
-                 if model.y_pos >= -1.0 then
-                   { model with y_pos = model.y_pos -. 0.01 }
-                 else { model with dir = Down }
-             | Down ->
-                 if model.y_pos <= 1.0 then
-                   { model with y_pos = model.y_pos +. 0.01 }
-                 else { model with dir = Up });
-       |])
+  Row.space_around
+    [|
+      Align.widget ~x_shift:model.y_pos
+        (Rect.widget ~width:100 ~height:100 ~color:Raylib.Color.gray
+           (Conditional.exec (fun _ -> true) (fun model -> animate model)));
+    |]
 
 let () = run_app view initial
