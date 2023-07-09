aa054b9c4d1b459776f447464ba6d5bdf1f77e74
humza <humza@pop-os.localdomain>
2023-07-09 15:40:56 +0100

    replace example 2 with clearer, and add comments

--- bin/main.ml
+++ bin/main.ml
@@ -4,6 +4,8 @@ open Reyml
   Example 1.
  *)
 
+(* These keys uniquely identify each Indeterminate_progress_bar so
+   we can keep track of state (handled by framework) for each. *)
 let key = Indeterminate_progress_bar.key ()
 let key2 = Indeterminate_progress_bar.key ()
 let key3 = Indeterminate_progress_bar.key ()
@@ -25,27 +27,26 @@ let () = run_app view ()
 
 (*
   Example 2.
+  This uses a Determinate_progress_bar which takes a percentage from the model.
  *)
 
 open Reyml
 
-type model = { color : Raylib.Color.t }
+type model = { progress : float }
 
-let initial_model = { color = Raylib.Color.create 0 0 0 0 }
-
-let update model =
-  (* Get "red" from the Color.t in the model *)
-  let rgb = Raylib.Color.r model.color in
-  let rgb = rgb + 1 in
-  let color = Raylib.Color.create rgb rgb rgb rgb in
-  { color }
+let initial_model = { progress = 0.0 }
+let update model = { progress = model.progress +. 0.001 }
 
 let view model =
-  Center.widget
-    (Row.center
-       [|
-         Rect.widget ~width:500 ~height:500 ~color:model.color Empty;
-         Conditional.exec true update;
-       |])
+  Padding.all 100
+    (Center.widget
+       (Row.center
+          [|
+            (* Take progress from model. *)
+            Determinate_progress_bar.horizontal ~percent:model.progress
+              ~width:800 ();
+            (* If the progress is less than 1.0, call the update function on the model. *)
+            Conditional.exec (model.progress < 1.0) update;
+          |]))
 
 let () = run_app view initial_model
