75c696541880140265e87d434365fb5c2ed300c8
humza <humza@pop-os.localdomain>
2023-07-09 14:42:05 +0100

    add another example to bin/main.ml

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,9 @@
 open Reyml
 
+(*
+  Example 1.
+ *)
+
 let key = Indeterminate_progress_bar.key ()
 let key2 = Indeterminate_progress_bar.key ()
 let key3 = Indeterminate_progress_bar.key ()
@@ -18,3 +22,31 @@ let view model =
        |])
 
 let () = run_app view ()
+
+(*
+  Example 2.
+  (Comment out the above code, and uncomment the code below.)
+ *)
+
+open Reyml
+
+type model = { color : Raylib.Color.t }
+
+let initial_model = { color = Raylib.Color.create 0 0 0 0 }
+
+let update model =
+  (* Get "red" from the Color.t in the model *)
+  let rgb = Raylib.Color.r model.color in
+  let rgb = rgb + 1 in
+  let color = Raylib.Color.create rgb rgb rgb rgb in
+  { color }
+
+let view model =
+  Center.widget
+    (Row.center
+       [|
+         Rect.widget ~width:500 ~height:500 ~color:model.color Empty;
+         Conditional.exec true update;
+       |])
+
+let () = run_app view initial_model
