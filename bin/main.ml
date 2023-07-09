f83058d9ac8e5e705cf6bb2e575909b2b1325cb3
humza <humza@pop-os.localdomain>
2023-07-09 05:13:52 +0100

    done with determinate progress bar

--- bin/main.ml
+++ bin/main.ml
@@ -5,7 +5,14 @@ type model = { percent : float }
 let initial = { percent = 0.0 }
 let animate model = { percent = model.percent +. 0.01 }
 
+let key1 = Indeterminate_progress_bar.key()
+let key2 = Indeterminate_progress_bar.key()
 let view model =
-  Center.widget (Determinate_progress_bar.vertical ~percent:1.0 ())
+  Column.space_around ~collapse_width:false
+  [|
+    Conditional.exec (fun _ -> true) animate;
+    Indeterminate_progress_bar.vertical key1;
+    Indeterminate_progress_bar.horizontal key2;
+  |]
 
 let () = run_app view initial
