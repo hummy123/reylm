5fde6bceb68634a7e05a2ef72d819e367af0fd45
Humza Shahid <humzashahid@Humzas-Mac-mini.broadband>
2023-07-11 00:09:23 +0100

    update example in bin/main.ml

--- bin/main.ml
+++ bin/main.ml
@@ -3,8 +3,9 @@ open Reyml.Layout
 open Reyml.Controls.Default
 open Reyml.Controls.Templates
 
+(* Create a new Indeterminate_progress_bar type using a functor.*)
 module Red_template = struct
-  type key = int
+  type key = string
 
   let default_background = Raylib.Color.create 214 214 214 255
   let default_foreground = Raylib.Color.create 255 67 67 255
@@ -15,8 +16,10 @@ end
 
 module Red_bar = Indeterminate_progress_bars.Make (Red_template)
 
+(* Register the bar's internal state with the framework. This is mutable, but that's fine. *)
 let _ = register [| Red_bar.did_change |]
 
+(* The model is just a record with an integer, and the update function adds 1 to it. *)
 type model = { red_delay : int }
 
 let initial_model = { red_delay = 0 }
@@ -26,9 +29,11 @@ let view model =
   Column.space_around
     [|
       Indeterminate_progress_bar.horizontal ~key:"asdf" ();
-      (if model.red_delay > 10 then Red_bar.horizontal ~key:1 () else Empty);
-      Conditional.exec true update;
-      Indeterminate_progress_bar.horizontal ~key:"awer" ();
+      (* The if statement below displays a red progress bar with the same key but a different state,
+         because defining a new functor resets the state. *)
+      (if model.red_delay > 10 then Red_bar.horizontal ~key:"asdf" () else Empty);
+      (* Conditional.exec just calls a function to update the model whenever some condition is true. *)
+      Conditional.exec (model.red_delay <= 10) update;
     |]
 
 let () = run_app view initial_model
